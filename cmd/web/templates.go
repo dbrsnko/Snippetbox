6e8abb34361eef8eb14159e8cb5a4f0f67ef8593
dbrsnko <contactdenysborysenko@gmail.com>
2024-06-05 18:43:21 +0300

    Secure headers

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,11 +4,21 @@ import (
 	"html/template"
 	"path/filepath"
 	"snippetbox.denysb/internal/models"
+	"time"
 )
 
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -24,7 +34,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}

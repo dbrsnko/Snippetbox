a5aa2efc4da0696956cbe8cccbcf503db3b1f4ce
dbrsnko <contactdenysborysenko@gmail.com>
2024-07-18 18:26:11 +0300

    Routes setup, user login and signup placeholders

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -27,6 +27,16 @@ func (app *application) routes() http.Handler {
 		dynamic.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create",
 		dynamic.ThenFunc(app.snippetCreatePost))
+	router.Handler(http.MethodGet, "/user/signup",
+		dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup",
+		dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login",
+		dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login",
+		dynamic.ThenFunc(app.userLoginPost))
+	router.Handler(http.MethodPost, "/user/logout",
+		dynamic.ThenFunc(app.userLogoutPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 

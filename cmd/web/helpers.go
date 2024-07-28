9ff406b70dcedf93b1699584b9d25ae09db16f5e
dbrsnko <contactdenysborysenko@gmail.com>
2024-07-28 17:01:10 +0300

    request context, embedded FS

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,13 +5,18 @@ import (
 	"errors"
 	"fmt"
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 	"net/http"
 	"runtime/debug"
 	"time"
 )
 
 func (app *application) isAuthenticated(r *http.Request) bool {
-	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+	return isAuthenticated
 }
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -34,6 +39,7 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+	"net/http"
+	"snippetbox.denysb/ui"
 )
 
 func (app *application) routes() http.Handler {
@@ -14,11 +14,10 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath",
-		http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id",

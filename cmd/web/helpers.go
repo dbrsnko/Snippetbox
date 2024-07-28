f8bb93bf50dee3f5b9681abdf9a7ae832f7e795e
dbrsnko <contactdenysborysenko@gmail.com>
2024-07-28 15:50:09 +0300

    login and logout functionality, safety imrovements

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -10,6 +10,10 @@ import (
 	"time"
 )
 
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+}
+
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
@@ -27,8 +31,9 @@ func (app *application) notFound(w http.ResponseWriter) {
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
